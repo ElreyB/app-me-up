a437123d8563f7ef8e28fd16582c93f4f44401e4
Elrey Belmonti <elbelmonti@gmail.com>
2020-01-24 08:50:04 -0800

    add html templates

---
+++ templates/html.js
@@ -0,0 +1,26 @@
+const html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>`;
+
+const htmlWithScript = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <script src="script.js"></script>
+    <title>Document</title>
+  </head>
+  <body>
+  </body>
+</html>
+`;
