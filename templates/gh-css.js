90dd636ddd42d37dae1ac9e16d9148015c4cac75
Elrey Belmonti <elbelmonti@gmail.com>
2020-01-25 00:06:59 -0800

    add css template

---
+++ templates/gh-css.js
@@ -0,0 +1,26 @@
+module.exports = {
+  githubCss: `
+  body {
+    background-color: black;
+  }
+
+  main {
+    margin-top: 50px;
+  }
+
+  .card-body {
+    background-color: #343a40;
+    color: white;
+  }
+
+  .card-body a:hover {
+    color: white;
+  }
+
+  .btn-info {
+    color: black;
+    font-weight: bold;
+  }
+
+  `
+};
